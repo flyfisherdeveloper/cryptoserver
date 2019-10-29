cef7dc56f00c17313b2d346d19c7c0b4f1ca44fa
flyfisherdeveloper <jbcamomile@gmail.com>
2019-10-29 16:22:29 -0600

    Add tests. Rename test.

---
+++ src/test/java/com/scanner/myscanner/MyScannerControllerIntegTest.java
@@ -0,0 +1,92 @@
+package com.scanner.myscanner;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scanner.myscanner.exchange.binance.us.controller.BinanceExchangeController;
+import com.scanner.myscanner.exchange.binance.us.dto.CoinDataFor24Hr;
+import com.scanner.myscanner.exchange.binance.us.dto.CoinTicker;
+import com.scanner.myscanner.exchange.binance.us.dto.ExchangeInfo;
+import com.scanner.myscanner.exchange.binance.us.dto.Symbol;
+import com.scanner.myscanner.exchange.binance.us.service.ExchangeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {BinanceExchangeController.class, ExchangeService.class, RestTemplate.class})
+@WebMvcTest
+class MyScannerControllerIntegTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void test_exchangeInfo() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/binance/info")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+
+        ExchangeInfo info = mapper.readValue(json, ExchangeInfo.class);
+        assertNotNull(info);
+        assertNotNull(info.getSymbols());
+        List<Symbol> symbols = info.getSymbols();
+        assertTrue(symbols.size() > 0);
+
+        //verify that bitcoin exists
+        Optional<Symbol> btcUsd = symbols.stream().filter(s -> s.getSymbol().equals("BTCUSD")).findFirst();
+        assertTrue(btcUsd.isPresent());
+        //verify that the symbol is indeed bitcoin
+        assertTrue(btcUsd.filter(b -> b.getBaseAsset().equals("BTC")).isPresent());
+        //verify that the symbol is paired with USD
+        assertTrue(btcUsd.filter(b -> b.getQuoteAsset().equals("USD")).isPresent());
+    }
+
+    @Test
+    void test_coin_for_24_ticker() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/binance/24HourTicker/LTCUSD")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+
+        CoinDataFor24Hr coin = mapper.readValue(json, CoinDataFor24Hr.class);
+        assertNotNull(coin);
+        assertEquals("LTCUSD", coin.getSymbol());
+    }
+
+    @Test
+    void test_coin_ticker() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/binance/ticker/DOGEUSDT/12h")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<CoinTicker> list = mapper.readValue(json, new TypeReference<>() {
+        });
+        assertNotNull(list);
+        assertFalse(list.isEmpty());
+        CoinTicker coinTicker = list.get(0);
+        assertNotNull(coinTicker.getVolume());
+        assertNotNull(coinTicker.getQuoteAssetVolume());
+    }
+}
--- src/test/java/com/scanner/myscanner/MyScannerServiceIntegTest.java
+++ src/test/java/com/scanner/myscanner/MyScannerServiceIntegTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class MyScannerIntegTest {
+class MyScannerServiceIntegTest {
 	@Autowired
 	private ExchangeService exchangeService;
 
