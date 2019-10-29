ca1af12e32d4e73b8626b3d510b982138eb6560c
flyfisherdeveloper <jbcamomile@gmail.com>
2019-10-29 15:19:44 -0600

    Add ticker endpoints. Clarify binance endpoint for controller.

--- src/main/java/com/scanner/myscanner/exchange/binance/us/controller/BinanceExchangeController.java
+++ src/main/java/com/scanner/myscanner/exchange/binance/us/controller/BinanceExchangeController.java
@@ -1,12 +1,19 @@
 package com.scanner.myscanner.exchange.binance.us.controller;
 
+import com.scanner.myscanner.exchange.binance.us.dto.CoinDataFor24Hr;
+import com.scanner.myscanner.exchange.binance.us.dto.CoinTicker;
 import com.scanner.myscanner.exchange.binance.us.dto.ExchangeInfo;
 import com.scanner.myscanner.exchange.binance.us.service.ExchangeService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("binance")
 public class BinanceExchangeController {
     private final ExchangeService service;
 
@@ -14,8 +21,18 @@ public class BinanceExchangeController {
         this.service = service;
     }
 
-    @GetMapping(value = "/binanceInfo", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
     public ExchangeInfo getExchangeInfo() {
         return service.getExchangeInfo();
+    }
+
+    @GetMapping(value = "/24HourTicker/{symbol}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CoinDataFor24Hr get24HourTicker(@PathVariable String symbol) {
+        return service.get24HrCoinTicker(symbol);
+    }
+
+    @GetMapping(value = "/ticker/{symbol}/{interval}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CoinTicker> getCoinTicker(@PathVariable String symbol, @PathVariable String interval) {
+        return service.getCoinTicker(symbol, interval);
     }
 }
