0739bb1ed9f2f628d987030ae36de6f067097bb7
flyfisherdeveloper <jbcamomile@gmail.com>
2019-10-30 16:41:38 -0600

    Add some mock service. Allow cross origin access for development.

--- src/main/java/com/scanner/myscanner/exchange/binance/us/controller/BinanceExchangeController.java
+++ src/main/java/com/scanner/myscanner/exchange/binance/us/controller/BinanceExchangeController.java
@@ -3,12 +3,10 @@ package com.scanner.myscanner.exchange.binance.us.controller;
 import com.scanner.myscanner.exchange.binance.us.dto.CoinDataFor24Hr;
 import com.scanner.myscanner.exchange.binance.us.dto.CoinTicker;
 import com.scanner.myscanner.exchange.binance.us.dto.ExchangeInfo;
+import com.scanner.myscanner.exchange.binance.us.dto.Symbol;
 import com.scanner.myscanner.exchange.binance.us.service.ExchangeService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,16 +20,22 @@ public class BinanceExchangeController {
     }
 
     @GetMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ExchangeInfo getExchangeInfo() {
-        return service.getExchangeInfo();
+    @CrossOrigin
+    public List<Symbol> getExchangeInfo() {
+        //return service.getExchangeInfo();
+        ExchangeInfo info = service.getMockExchangeInfo();
+        return info.getSymbols();
     }
 
     @GetMapping(value = "/24HourTicker/{symbol}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin
     public CoinDataFor24Hr get24HourTicker(@PathVariable String symbol) {
-        return service.get24HrCoinTicker(symbol);
+        //return service.get24HrCoinTicker(symbol);
+        return service.getMock24HrCoinTicker(symbol);
     }
 
     @GetMapping(value = "/ticker/{symbol}/{interval}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin
     public List<CoinTicker> getCoinTicker(@PathVariable String symbol, @PathVariable String interval) {
         return service.getCoinTicker(symbol, interval);
     }
