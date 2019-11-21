860a19c46411c4f8c5edc1a2de068ca76c57ce75
Jeffrey Camomile <jeffrey.camomile@zionsbancorp.com>
2019-11-21 14:16:41 -0700

    Add zip file with icons - add code to extract icons to service

--- src/main/java/com/scanner/myscanner/CachingConfig.java
+++ src/main/java/com/scanner/myscanner/CachingConfig.java
@@ -36,6 +36,14 @@ public class CachingConfig extends CachingConfigurerSupport {
                             .asMap(),
                             false);
                 }
+                if (name.equals("IconCache")) {
+                    return new ConcurrentMapCache(name, CacheBuilder.newBuilder()
+                            .expireAfterWrite(24, TimeUnit.HOURS)
+                            .maximumSize(1000)
+                            .build()
+                            .asMap(),
+                            false);
+                }
                 //cache for list of all coins
                 return new ConcurrentMapCache(name, CacheBuilder.newBuilder()
                         .expireAfterWrite(30, TimeUnit.MINUTES)
