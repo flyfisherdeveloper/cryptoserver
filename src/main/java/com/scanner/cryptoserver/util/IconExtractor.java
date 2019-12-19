46d9b77b65bd59f70a0a9849f63aa732fba7cb2c
flyfisherdeveloper <jbcamomile@gmail.com>
2019-12-18 17:42:13 -0700

    Add logging.

--- src/main/java/com/scanner/cryptoserver/util/IconExtractor.java
+++ src/main/java/com/scanner/cryptoserver/util/IconExtractor.java
@@ -1,32 +1,43 @@
 package com.scanner.cryptoserver.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.*;
 
 public class IconExtractor {
+    private static final Logger Log = LoggerFactory.getLogger(IconExtractor.class);
 
     /**
      * Extract a coin icon from a zip file.
+     *
      * @param coin the coin name.
      * @return a byte array of the png icon file.
-     * @throws IOException on exception (usually when the png is not in the zip file).
      */
-    public static byte[] getIconBytes(String coin) throws IOException {
+    public static byte[] getIconBytes(String coin) {
         URL url = IconExtractor.class.getClassLoader().getResource("cryptocurrency-icons-master.zip");
         assert url != null;
-        Path zipFile = null;
+        Path zipFile;
+
         try {
             zipFile = Paths.get(url.toURI());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            Log.error("Cannot open icons zip file. Coin: {}", coin, e);
+            return null;
         }
-        assert zipFile != null;
-        FileSystem fs = FileSystems.newFileSystem(zipFile, ClassLoader.getSystemClassLoader());
-        final Path icon = fs.getPath("/cryptocurrency-icons-master/32/color/" + coin + ".png");
-        byte[] bytes = Files.readAllBytes(icon);
-        fs.close();
+
+        FileSystem fs;
+        byte[] bytes = null;
+        try {
+            fs = FileSystems.newFileSystem(zipFile, ClassLoader.getSystemClassLoader());
+            final Path icon = fs.getPath("/cryptocurrency-icons-master/32/color/" + coin + ".png");
+            bytes = Files.readAllBytes(icon);
+            fs.close();
+        } catch (IOException e) {
+            Log.error("Cannot extract png from icons zip file. Coin: {}", coin, e);
+        }
         return bytes;
     }
 }
