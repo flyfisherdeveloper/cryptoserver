92881b8ebd073d5604d2da61a2045db724c3b503
Jeffrey Camomile <jeffrey.camomile@zionsbancorp.com>
2019-12-24 17:26:53 -0700

    Use Spring's resource resolver to extract icons from the jar file. Add Groovy tests.

--- src/main/java/com/scanner/cryptoserver/util/IconExtractor.java
+++ src/main/java/com/scanner/cryptoserver/util/IconExtractor.java
@@ -2,6 +2,9 @@ package com.scanner.cryptoserver.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,13 +19,18 @@ public class IconExtractor {
      * @return a byte array of the png icon file.
      */
     public static byte[] getIconBytes(String coin) {
-        //try to get the icon from the resources - known icons are stored there
-        InputStream stream = IconExtractor.class.getClassLoader().getResourceAsStream("icons/" + coin + ".png");
-        if (stream == null) {
-            return null;
-        }
+        ClassLoader cl = IconExtractor.class.getClassLoader();
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+        Resource resource;
         byte[] bytes = null;
-        try {
+
+        //try to get the icon from the resources - known icons are stored there
+        resource = resolver.getResource("classpath:icons/" + coin.toLowerCase() + ".png");
+        try (InputStream stream = resource.getInputStream()) {
+            if (stream == null) {
+                Log.error("Cannot load input stream for png file: Coin: {}", coin);
+                return null;
+            }
             bytes = new byte[stream.available()];
             stream.read(bytes);
         } catch (IOException e) {
