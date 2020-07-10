0d7c883e45b5ae75d37a4ba46e03c3c9312c2fb7
Jeff Camomile <jbcamomile@gmail.com>
2020-07-10 15:10:21 -0600

    Add Bittrex sandbox controller test

--- src/test/java/com/scanner/cryptoserver/exchange/bittrex/service/SandboxBittrexTest.groovy
+++ src/test/java/com/scanner/cryptoserver/exchange/bittrex/service/SandboxBittrexTest.groovy
@@ -1,21 +1,36 @@
 package com.scanner.cryptoserver.exchange.bittrex.service
 
-
+import com.scanner.cryptoserver.exchange.bittrex.controller.SandboxBittrexExchangeController
 import com.scanner.cryptoserver.util.SandboxUtil
 import spock.lang.Specification
 
-class SandboxBittrexExchangeServiceTest extends Specification {
+class SandboxBittrexTest extends Specification {
     private SandboxBittrexExchangeService service
+    private SandboxBittrexExchangeController controller
     private SandboxUtil sandboxUtil
 
     def setup() {
         sandboxUtil = new SandboxUtil()
         service = new SandboxBittrexExchangeService(sandboxUtil)
+        controller = new SandboxBittrexExchangeController(service)
     }
 
     def "test get24HrAllCoinTicker"() {
         when:
           def tickers = service.get24HrAllCoinTicker()
+
+        then:
+          assert tickers
+          assert tickers.size() > 0
+          //make sure we find one that we know is in there
+          def mtlBtc = tickers.find { it.symbol == "MTL-BTC" }
+          assert mtlBtc
+          assert mtlBtc.getHighPrice() > mtlBtc.getLowPrice()
+    }
+
+    def "test get24HrAllCoinTicker from controller"() {
+        when:
+          def tickers = controller.getAll24HourTicker()
 
         then:
           assert tickers
