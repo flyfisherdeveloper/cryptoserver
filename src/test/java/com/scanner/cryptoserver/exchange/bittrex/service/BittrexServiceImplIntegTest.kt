c4fbde32e0ab35fc66df6838bd72c544506861e4
Jeff Camomile <jbcamomile@gmail.com>
2020-07-13 14:14:36 -0600

    Fix Bittrex integ test

--- src/test/java/com/scanner/cryptoserver/exchange/bittrex/service/BittrexServiceImplIntegTest.kt
+++ src/test/java/com/scanner/cryptoserver/exchange/bittrex/service/BittrexServiceImplIntegTest.kt
@@ -1,16 +1,20 @@
 package com.scanner.cryptoserver.exchange.bittrex.service
 
 import com.scanner.cryptoserver.CachingConfig
+import com.scanner.cryptoserver.exchange.coinmarketcap.CoinMarketCapApiServiceImpl
+import com.scanner.cryptoserver.exchange.coinmarketcap.CoinMarketCapService
 import com.scanner.cryptoserver.util.CacheUtilImpl
-import junit.framework.Assert.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.client.RestTemplate
 
 //Here, we load only the components needed - this prevents a full Spring Boot test from running, as only certain components are needed.
 //For example, startup initialization threads are not needed, etc.
-@ContextConfiguration(classes = [CachingConfig::class, CacheUtilImpl::class, BittrexServiceImpl::class])
+@ContextConfiguration(classes = [CachingConfig::class, CacheUtilImpl::class, CoinMarketCapService::class,
+    CoinMarketCapApiServiceImpl::class, RestTemplate::class, BittrexServiceImpl::class])
 @WebMvcTest
 internal class BittrexServiceImplIntegTest(@Autowired private val service: BittrexServiceImpl) {
 
