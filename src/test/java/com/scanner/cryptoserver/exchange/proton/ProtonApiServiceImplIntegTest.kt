c6a174ebcc1cb64e1228a2227887d06845a9aa19
Jeff Camomile <jbcamomile@gmail.com>
2020-09-29 13:56:36 -0600

    Don't use deprecated import.

--- src/test/java/com/scanner/cryptoserver/exchange/proton/ProtonApiServiceImplIntegTest.kt
+++ src/test/java/com/scanner/cryptoserver/exchange/proton/ProtonApiServiceImplIntegTest.kt
@@ -1,7 +1,7 @@
 package com.scanner.cryptoserver.exchange.proton
 
 import com.scanner.cryptoserver.util.UrlReaderImpl
-import junit.framework.Assert.assertNotNull
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
